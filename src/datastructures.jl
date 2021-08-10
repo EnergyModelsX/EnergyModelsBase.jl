bbd18adc422fd92a0f98eba809e1be89273c7415
Dimitri Quentin Alexis Pinel <dimitri.pinel@sintef.no>
2021-08-10 11:00:28 +0200

    Changes for investment in storage capacity and power (see commit in investmentmodels.jl)

--- src/datastructures.jl
+++ src/datastructures.jl
@@ -82,7 +82,7 @@ end
 struct RefStorage <: Storage
     id
     capacity::TimeProfile
-    cap_storage::Real
+    cap_storage::TimeProfile
     var_opex::TimeProfile
     fixed_opex::TimeProfile
     input::Dict{Resource, Real}
