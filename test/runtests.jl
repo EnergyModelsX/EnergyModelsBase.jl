8105a3c534cc610e490ae290750f65f1cde5f234
Sigmund Eggen Holm <sigmund.holm@sintef.no>
2022-05-24 09:33:41 +0200

    Refactor the tests out in a separate file.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,10 @@
-using EnergyModelsBase
 using Test
-using TimeStructures
-using JuMP
-using GLPK
 
-const EMB = EnergyModelsBase
+using EnergyModelsBase
 
-m, case = EMB.run_model("",GLPK.Optimizer)
 
-@testset "User interface" begin
-    # Check for the objective value
-    @test objective_value(m) ≈ 129056.305
+@testset "EnergyModelsBase" begin
 
-    # Check for the total number of variables
-    @test size(all_variables(m))[1] == 7160
+    include("user_interface.jl")
 
-    # Check for total emissions of both methane and CO2
-    global_data = case[:global_data]
-    CH4         = case[:products][1]
-    CO2         = case[:products][4]
-    𝒯ᴵⁿᵛ        = strategic_periods(case[:T])
-    for t_inv ∈ 𝒯ᴵⁿᵛ
-        @test value.(m[:emissions_strategic])[t_inv,CO2] <= global_data.Emission_limit[CO2][t_inv]
-        @test value.(m[:emissions_strategic])[t_inv,CH4] <= global_data.Emission_limit[CH4][t_inv]
-    end
 end
---
+++ test/user_interface.jl
@@ -0,0 +1,30 @@
+
+using TimeStructures
+using JuMP
+using GLPK
+
+const EMB = EnergyModelsBase
+
+
+@testset "User interface" begin
+    m, case = EMB.run_model("", GLPK.Optimizer)
+
+    # Check for the objective value
+    @test objective_value(m) ≈ 129056.305
+
+    # Check for the total number of variables
+    @test size(all_variables(m))[1] == 7160
+
+    # Check for total emissions of both methane and CO2
+    global_data = case[:global_data]
+    CH4 = case[:products][1]
+    CO2 = case[:products][4]
+    𝒯ᴵⁿᵛ = strategic_periods(case[:T])
+
+    @test sum(value.(m[:emissions_strategic])[t_inv, CO2]
+              <=
+              global_data.Emission_limit[CO2][t_inv] for t_inv ∈ 𝒯ᴵⁿᵛ) == length(𝒯ᴵⁿᵛ)
+    @test sum(value.(m[:emissions_strategic])[t_inv, CH4]
+              <=
+              global_data.Emission_limit[CH4][t_inv] for t_inv ∈ 𝒯ᴵⁿᵛ) == length(𝒯ᴵⁿᵛ)
+end
