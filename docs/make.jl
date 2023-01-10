dfb26b70ec5b6c4fb72759982aa0ae2256fcd0f2
Sigmund Eggen Holm <sigmund.holm@sintef.no>
2023-01-10 10:04:05 +0100

    Remove redundant ssh-folder setup from ci file.
    Add news file as a page in the docs.

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,13 @@ using Documenter
 using EnergyModelsBase
 const EMB = EnergyModelsBase
 
+# Copy the NEWS.md file
+news = "src/manual/NEWS.md"
+if isfile(news)
+    rm(news)
+end
+cp("../NEWS.md", "src/manual/NEWS.md")
+
 makedocs(
     sitename = "EnergyModelsBase",
     format = Documenter.HTML(),
@@ -13,6 +20,7 @@ makedocs(
             "Quick Start" => "manual/quick-start.md",
             "Philosophy" => "manual/philosophy.md",
             "Example" => "manual/simple-example.md",
+            "Release notes" => "manual/NEWS.md",
         ],
         "Library" => Any[
             "Public" => "library/public.md",
