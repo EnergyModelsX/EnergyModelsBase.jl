a05cee6d79560b1519d3d8319685f65f0b1ad292
Julian Straus <julian.straus@sintef.no>
2024-01-26 09:43:37 +0100

    Third test regarding documentation

--- docs/make.jl
+++ docs/make.jl
@@ -9,17 +9,15 @@ news = "src/manual/NEWS.md"
 if isfile(news)
     rm(news)
 end
-cp("../NEWS.md", "src/manual/NEWS.md")
+cp("../NEWS.md", news)
 
 
 DocMeta.setdocmeta!(EnergyModelsBase, :DocTestSetup, :(using EnergyModelsBase); recursive=true)
 
 makedocs(
     sitename = "EnergyModelsBase.jl",
-    repo="https://gitlab.sintef.no/clean_export/energymodelsbase.jl/blob/{commit}{path}#{line}",
     format = Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://clean_export.pages.sintef.no/energymodelsbase.jl/",
         edit_link="main",
         assets=String[],
     ),
@@ -29,7 +27,6 @@ makedocs(
         "Manual" => Any[
             "Quick Start" => "manual/quick-start.md",
             "Philosophy" => "manual/philosophy.md",
-            # "Nodes" => "manual/nodes.md",
             "Optimization variables" => "manual/optimization-variables.md",
             "Constraint functions" => "manual/constraint-functions.md",
             "Data functions" => "manual/data-functions.md",
@@ -52,6 +49,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "https://github.com/EnergyModelsX/EnergyModelsBase.jl"
+)
