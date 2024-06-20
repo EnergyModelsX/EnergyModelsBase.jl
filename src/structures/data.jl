f48d28e4df7c30b4049057ae5cd2e31459e3d430
Julian Straus <julian.straus@sintef.no>
2024-06-20 09:12:59 +0200

    Removed TransInvData constructor

--- src/structures/data.jl
+++ src/structures/data.jl
@@ -233,33 +233,6 @@ for further information regarding how you can translate your existing model to t
 InvData(nothing) = nothing
 
 """
-    TransInvData(;
-        capex_trans::TimeProfile,
-        trans_max_inst::TimeProfile,
-        trans_max_add::TimeProfile,
-        trans_min_add::TimeProfile,
-        inv_mode::Investment = ContinuousInvestment(),
-        trans_start::Union{Real, Nothing} = nothing,
-        trans_increment::TimeProfile = FixedProfile(0),
-        capex_trans_offset::TimeProfile = FixedProfile(0),
-    )
-
-Legacy constructor for a `InvData`.
-
-The new storage descriptions allows now for a reduction in functions which is used
-to make `EnergModelsInvestments` less dependent on `EnergyModelsBase`.
-
-The core changes to the existing structure is the move of the required parameters to the
-type [`Investment`](@ref) (_e.g._, the minimum and maximum added capacity is only required
-for investment mdodes that require these parameters) as well as moving the `lifetime` to the
-type [`LifetimeMode`], when required.
-
-See the _[documentation](https://energymodelsx.github.io/EnergyModelsInvestments.jl/stable/how-to/update-models)_
-for further information regarding how you can translate your existing model to the new model.
-"""
-TransInvData(nothing) = nothing
-
-"""
     InvDataStorage(;
         #Investment data related to storage power
         capex_rate::TimeProfile,
